b631320ddec920f01adf57583bef87994b67230d
Kevin Harrington <mad.hephaestus@gmail.com>
2022-02-13 20:42:34 -0500

    dimensions
    square it up

--- cube.groovy
+++ cube.groovy
@@ -1,5 +1,5 @@
 
-return new Cube(	40,// X dimention
-			60,// Y dimention
-			80//  Z dimention
+return new Cube(	45,// X dimention
+			45,// Y dimention
+			45//  Z dimention
 			).toCSG()// this converts from the geometry to an object we can work with
